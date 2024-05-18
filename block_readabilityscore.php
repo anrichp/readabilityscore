725f2011f40e13bf496027bbbbfdcbbecd80dbb6
Anrich Potgieter <anrichp@gmail.com>
2024-05-18 10:07:34 +0000

    importing moodle boilerplate for blocks

--- block_readabilityscore.php
+++ block_readabilityscore.php
@@ -1,31 +1,89 @@
 <?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
-defined('MOODLE_INTERNAL') || die();
+/**
+ * Block readabilityscore is defined here.
+ *
+ * @package     block_readabilityscore
+ * @copyright   2024 Anrich Potgieter <anrichp@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class block_readabilityscore extends block_base {
 
-function get_content() {
-    global $PAGE;
-
-    // Check if user has required privileges
-    if (!has_capability('moodle/site:config', context_system::instance())) {
-        return '';
+    /**
+     * Initializes class member variables.
+     */
+    public function init() {
+        // Needed by Moodle to differentiate between blocks.
+        $this->title = get_string('pluginname', 'block_readabilityscore');
     }
 
-    // Get current page content
-    $pagecontent = $PAGE->get_course()->summary;
+    /**
+     * Returns the block contents.
+     *
+     * @return stdClass The block contents.
+     */
+    public function get_content() {
 
-    // Extract text from page content
-    $text = strip_tags($pagecontent);
+        if ($this->content !== null) {
+            return $this->content;
+        }
 
-    // Send text to readability score function
-    $score = readability_score($text);
+        if (empty($this->instance)) {
+            $this->content = '';
+            return $this->content;
+        }
 
-    // Display readability score
-    $this->content = new stdClass();
-    $this->content->text = 'Readability Score: ' . $score;
-    $this->content->footer = '';
-}
+        $this->content = new stdClass();
+        $this->content->items = array();
+        $this->content->icons = array();
+        $this->content->footer = '';
 
-function readability_score($text) {
-    // Implement readability score calculation logic here
-    // Return the score as a string (e.g., 'Grade 8.5')
+        if (!empty($this->config->text)) {
+            $this->content->text = $this->config->text;
+        } else {
+            $text = 'Please define the content text in /blocks/readabilityscore/block_readabilityscore.php.';
+            $this->content->text = $text;
+        }
+
+        return $this->content;
+    }
+
+    /**
+     * Defines configuration data.
+     *
+     * The function is called immediately after init().
+     */
+    public function specialization() {
+
+        // Load user defined title and make sure it's never empty.
+        if (empty($this->config->title)) {
+            $this->title = get_string('pluginname', 'block_readabilityscore');
+        } else {
+            $this->title = $this->config->title;
+        }
+    }
+
+    /**
+     * Sets the applicable formats for the block.
+     *
+     * @return string[] Array of pages and permissions.
+     */
+    public function applicable_formats() {
+        return array(
+        );
+    }
 }
