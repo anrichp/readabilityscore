727f2eda2cb87108fe6da35811e030d08ccd3a53
Anrich Potgieter <anrichp@gmail.com>
2024-05-18 10:02:07 +0000

    Update function to comply with newer moodle

--- block_readabilityscore.php
+++ block_readabilityscore.php
@@ -2,34 +2,27 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-class block_readabilityscore extends block_base {
-    function init() {
-        $this->title = get_string('pluginname', 'block_readabilityscore');
-        $this->version = 2024051800; // Version number
+function get_content() {
+    global $PAGE;
+
+    // Check if user has required privileges
+    if (!has_capability('moodle/site:config', context_system::instance())) {
+        return '';
     }
 
-    function get_content() {
-        global $PAGE;
+    // Get current page content
+    $pagecontent = $PAGE->get_course()->summary;
 
-        // Check if user has required privileges
-        if (!has_capability('moodle/site:config', context_system::instance())) {
-            return '';
-        }
+    // Extract text from page content
+    $text = strip_tags($pagecontent);
 
-        // Get current page content
-        $pagecontent = $PAGE->get_renderer('core', 'page')->render_page($PAGE);
+    // Send text to readability score function
+    $score = readability_score($text);
 
-        // Extract text from page content
-        $text = strip_tags($pagecontent);
-
-        // Send text to readability score function
-        $score = readability_score($text);
-
-        // Display readability score
-        $this->content = new stdClass();
-        $this->content->text = 'Readability Score: ' . $score;
-        $this->content->footer = '';
-    }
+    // Display readability score
+    $this->content = new stdClass();
+    $this->content->text = 'Readability Score: ' . $score;
+    $this->content->footer = '';
 }
 
 function readability_score($text) {
