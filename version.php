83ddc5d1985b5286b5804bfc2bb94df9e98e631f
Anrich Potgieter <anrichp@gmail.com>
2024-05-18 09:53:24 +0000

    create version file for block

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2024051800;
+$plugin->component = 'block_readabilityscore';
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->requires  = 2023100400;
