d08ac71304b5aa0aa8ca88ba7c2636e1d6a95cc2
Anrich Potgieter <anrichp@gmail.com>
2024-05-19 18:22:57 +0000

    update version to trigger update

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_readabilityscore';
-$plugin->release = '0.1.0';
-$plugin->version = 2024051800;
+$plugin->release = '0.1.1';
+$plugin->version = 2024051900;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
