ac59034e55261025eabcee4a8f20fdb8f0718eab
Anrich Potgieter <anrichp@gmail.com>
2024-06-02 15:04:06 +0000

    amend button code

--- block_readabilityscore.php
+++ block_readabilityscore.php
@@ -80,12 +80,7 @@ class block_readabilityscore extends block_base
         // Add button to open dashboard page
         $dashboardButton = new single_button(
             new moodle_url('/blocks/readabilityscore/dashboard.php'), 'Dashboard');
-        $scanButton = new single_button(
-            new moodle_url('#'), // Replace with the correct URL if needed
-            'Scan', // Button text
-            'post', // Method
-            single_button::BUTTON_PRIMARY, // Button style
-            ['id' => 'scan-button', 'class' => 'block_readabilityscore_scanbutton'] // Additional attributes
+        $scanButton = new single_button(new moodle_url('#'), 'Scan', 'post', ['id' => 'scan-button', 'class' => 'block_readabilityscore_scanbutton']
         );
         $this->content->text .= html_writer::tag('div', $OUTPUT->render($dashboardButton));
         // Render the button and append it to the content
