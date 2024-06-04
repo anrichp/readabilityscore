ec7fdff6306ca00fb4c6e33e2a5bcd51deb18e99
Anrich Potgieter <anrichp@gmail.com>
2024-06-04 21:25:38 +0000

    update to store page url and selected text

--- db/install.php
+++ db/install.php
@@ -8,17 +8,19 @@
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-function xmldb_block_readabilityscore_install() {
+ function xmldb_block_readabilityscore_install() {
     global $DB;
     
     // Define table readability_scores to be created
     $table = new xmldb_table('readability_scores');
 
     // Adding fields to table readability_scores
-    $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
-    $table->add_field('userid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
-    $table->add_field('score', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
-    $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+    $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE);
+    $table->add_field('userid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL);
+    $table->add_field('score', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL);
+    $table->add_field('selectedtext', XMLDB_TYPE_TEXT, null, null, null); // Field for storing the selected text
+    $table->add_field('pageurl', XMLDB_TYPE_TEXT, null, null, null); // Field for storing the page URL
+    $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL);
 
     // Adding keys to table readability_scores
     $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
