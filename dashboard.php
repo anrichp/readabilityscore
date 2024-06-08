b908ac0251b425d2cbc1d455a872ba10b7be9022
Anrich Potgieter <anrichp@gmail.com>
2024-06-08 15:27:28 +0000

    revert back to working dashbaord without export

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Moodle - (link unavailable)
+// This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -12,11 +12,10 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <(link unavailable)>.
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 require_once(dirname(__FILE__) . '/../../config.php');
 require_once($CFG->dirroot . '/blocks/readabilityscore/lib.php');
-require_once('SimpleXLSXGen.php'); // Include SimpleXLSXGen.php
 
 $PAGE->set_url('/blocks/readabilityscore/dashboard.php');
 $PAGE->set_context(context_system::instance());
@@ -25,44 +24,39 @@ $PAGE->set_heading(get_string('pluginname', 'block_readabilityscore'));
 
 echo $OUTPUT->header();
 
+// Display the table describing the levels of readability difficulty
+$table = new html_table();
+$table->head = array('Score Range', 'Difficulty Level', 'Notes');
+$table->data = array(
+    array('90-100', 'Very Easy', 'Easily understood by an average 11-year-old student'),
+    array('80-89', 'Easy', 'Conversational English for consumers'),
+    array('70-79', 'Fairly Easy', 'Easily understood by 13- to 15-year-old students'),
+    array('60-69', 'Standard', 'Easily understood by 15- to 17-year-old students'),
+    array('50-59', 'Fairly Difficult', 'Fairly difficult to read'),
+    array('30-49', 'Difficult', 'College level and beyond'),
+    array('0-29', 'Very Confusing', 'Best understood by university graduates'),
+);
+
+echo html_writer::tag('h2', 'Readability Difficulty Levels');
+echo html_writer::table($table);
+
 // Fetch all unique page URLs from the database
 $pageURLs = $DB->get_fieldset_select('readability_scores', 'DISTINCT pageurl', null, null, 'pageurl');
 
 if (!empty($pageURLs)) {
     // Display form with dropdown to select page URL
-    $urlSelect = html_writer::start_tag('form', array('method' => 'get'));
-    $urlSelect .= html_writer::tag('label', 'Select Page URL:', array('for' => 'page-url-select'));
-    $urlSelect .= html_writer::start_tag('select', array('id' => 'page-url-select', 'name' => 'page_url'));
+    $options = array();
     foreach ($pageURLs as $url) {
-        $urlSelect .= html_writer::tag('option', $url, array('value' => $url));
+        $options[$url] = $url;
     }
-    $urlSelect .= html_writer::end_tag('select');
-    $urlSelect .= html_writer::tag('button', 'View Scans');
-    $urlSelect .= html_writer::end_tag('form');
-    echo $urlSelect;
+    $select = html_writer::select($options, 'page_url', '', array('' => 'Select a page'), array('id' => 'page-url-select'));
+    $form = html_writer::tag(
+        'form',
+        html_writer::tag('label', 'Select Page URL:', array('for' => 'page-url-select')) . $select . html_writer::empty_tag('input', array('type' => 'submit', 'value' => 'View Scans')),
+        array('method' => 'get')
+    );
 
-    // Check if export to Excel button is clicked
-    if (isset($_GET['export_excel'])) {
-        // Fetch scans for the selected page URL
-        $selectedPageURL = optional_param('page_url', '', PARAM_TEXT);
-        $scans = $DB->get_records_select('readability_scores', 'pageurl = ?', array($selectedPageURL));
-
-        // Create data array for Excel
-        $data = [['User ID', 'Readability Score', 'Selected Text', 'Page URL', 'Time Created']];
-        foreach ($scans as $scan) {
-            $data[] = [
-                $scan->userid,
-                $scan->score,
-                $scan->selectedtext,
-                $scan->pageurl,
-                date('Y-m-d H:i:s', $scan->timecreated)
-            ];
-        }
-
-        // Generate and download Excel file
-        $xlsx = SimpleXLSXGen::fromArray($data);
-        $xlsx->downloadAs('scans.xlsx');
-    }
+    echo $form;
 
     // Check if a specific page URL is selected
     $selectedPageURL = optional_param('page_url', '', PARAM_TEXT);
@@ -78,7 +72,7 @@ if (!empty($pageURLs)) {
                 echo html_writer::tag('p', 'Selected Text: ' . $scan->selectedtext); // Display selected text
                 echo html_writer::tag('p', 'Page URL: ' . $scan->pageurl); // Display page URL
                 echo html_writer::tag('p', 'Time Created: ' . date('Y-m-d H:i:s', $scan->timecreated));
-                echo html_writer::tag('hr');
+                echo html_writer::empty_tag('hr');
             }
         } else {
             echo html_writer::tag('p', 'No scans found for the selected page URL.');
@@ -91,4 +85,3 @@ if (!empty($pageURLs)) {
 }
 
 echo $OUTPUT->footer();
-?>
